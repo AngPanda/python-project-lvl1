29e518bcc1e6e43700b8a90cdf365a5a16dc1e8c
Nikolay Nikonov <qnickp@gmail.com>
2020-02-01 23:02:43 +0300

    fix linter

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -4,15 +4,15 @@ import random
 
 
 def game():
-  greeting()
-  print(rules_games('even'))
-  name = welcome_user()
-  count = 0
+    greeting()
+    print(rules_games('even'))
+    name = welcome_user()
+    count = 0
 
-  while count < 3:
-    number = random.randint(1, 99)
-    answer = 'yes' if number % 2 == 0 else 'no'
-    user_answer = question(number)
-    count = check_answer(user_answer, answer, count, name)
+    while count < 3:
+        number = random.randint(1, 99)
+        answer = 'yes' if number % 2 == 0 else 'no'
+        user_answer = question(number)
+        count = check_answer(user_answer, answer, count, name)
 
-  print("Congratulations, {}!" .format(name))
+    print("Congratulations, {}!" .format(name))
