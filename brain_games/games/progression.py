96ab33b83e655d2bf51ed2e9a7034205f1a2649e
Nikolay Nikonov <qnickp@gmail.com>
2020-02-08 16:42:25 +0300

    add progression

---
+++ brain_games/games/progression.py
@@ -0,0 +1,18 @@
+from random import randint
+
+
+description = "What number is missing \033[1min\033[0m the progression?"
+
+
+def game_round():
+    step = randint(1, 10)
+    start_point = randint(1, 99)
+    end_point = start_point + step * 10
+    sequnce = list(range(start_point, end_point, step))
+    gap_index = randint(0, 9)
+    answer = str(sequnce[gap_index])
+    sequnce[gap_index] = '..'
+    question = ''
+    for element in sequnce:
+        question += str(element) + ' '
+    return(question, answer)
