149441b6b0293a39b71f92c16de0d8d9ec6f9543
Nikolay Nikonov <qnickp@gmail.com>
2020-02-11 00:16:30 +0300

    correction remarks

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,10 +1,11 @@
-from random import randint
+from random import randrange
 
 
 DESCRIPTION = 'Answer "yes" if number even otherwise answer "no".'
+MAX_NUMBERS = 100
 
 
 def game_round():
-    number = randint(1, 99)
+    number = randrange(MAX_NUMBERS)
     answer = 'yes' if number % 2 == 0 else 'no'
-    return(number, answer)
+    return (number, answer)
