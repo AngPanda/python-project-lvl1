0263685c6da8638dd31577552b6119c7ddc00f7a
Nikolay Nikonov <qnickp@gmail.com>
2020-02-15 06:43:27 +0300

    correction remarks

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,23 +4,25 @@ import prompt
 ATTEMPTS = 3
 
 
-def greeting(description=None):
+def greet(description=None):
     print("Welcome to the Brain Games!")
     if description is not None:
         print(description)
-    name = prompt.string('\nMay I have your name? ')
-    print(f'Hello, {name}!\n')
+        print()
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print()
     return name
 
 
 def run(game):
-    name = greeting(game.DESCRIPTION)
+    name = greet(game.DESCRIPTION)
     for i in range(ATTEMPTS):
         question, answer = game.game_round()
         user_answer = ask(question)
         if answer != user_answer:
-            print(f"{user_answer} is wrong answer ;(. Correct answer was {answer}.\
-                    \nLet's try again, {name}!")
+            print(f"{user_answer} is wrong answer ;(. Correct answer was {answer}.")
+            print(f"Let's try again, {name}!")
             break
         else:
             print("Correct!")
--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,14 +2,14 @@ from random import randrange, choice
 from operator import add, sub, mul
 
 DESCRIPTION = 'What is the result of the expression?'
-LIST_SIGN = (('+', add), ('-', sub), ('*', mul))
+LIST_SIGNS = (('+', add), ('-', sub), ('*', mul))
 MAX_NUMBER = 100
 
 
 def game_round():
     number_one = randrange(MAX_NUMBER)
     number_two = randrange(MAX_NUMBER)
-    random_sign = choice(LIST_SIGN)
-    answer = str(random_sign[1](number_one, number_two))
-    question = f'{number_one} {random_sign[0]} {number_two}'
+    sign, instruction = choice(LIST_SIGNS)
+    answer = str(instruction(number_one, number_two))
+    question = f'{number_one} {sign} {number_two}'
     return (question, answer)
